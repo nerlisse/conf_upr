2d1be1f609b6329a81f94e0d9e4d709cfdb99ddc
nerlisse <allertalka@gmail.com>
2024-12-09 23:14:02 +0300

    fixed tests hw3

--- homework3/test/TestConfigParser.py
+++ homework3/test/TestConfigParser.py
@@ -10,63 +10,60 @@ class TestConfigParser(unittest.TestCase):
 
     def test_table(self):
         input_text = """
-        table(
+        (def const table(
             key1 => 325,
             key2 => table(
                 nested_key => 2511,
             ),
             key3 => 914,
-        )
+        ) )
         """
         result = self.parser.process_config(input_text)
         #print(result)
-        expected = [{
+        expected = {'const':{
             'key1': 325,
             'key2': {'nested_key': 2511},
             'key3': 914
-        }]
+        }}
         self.assertEqual(yaml.safe_load(result), expected)
 
     def test_array(self):
         input_text = """
-        << 1, 2, table(key => 3), 4 >>
+        (def const << 1, 2, table(key => 3), 4 >>)
         """
         result = self.parser.process_config(input_text)
-        expected = [[1, 2, {'key': 3}, 4]]
+        expected = {'const': [1, 2, {'key': 3}, 4]}
         self.assertEqual(yaml.safe_load(result), expected)
 
     def test_undefined_constant(self):
         input_text = """
-        table(key => #[undefined_const])
+        (def const table( key => #[undefined_const], ) )
         """
         with self.assertRaises(ValueError):
             self.parser.process_config(input_text)
 
     def test_invalid_syntax(self):
         input_text = """
-        table(key => #![invalid])
+        (def const table(key => 43 )
         """
-        with self.assertRaises(ValueError):
+        with self.assertRaises(SyntaxError):
             self.parser.process_config(input_text)
 
     def test_program(self):
         input_text = """
-        (def myconst table(
-            leeknow => 2511,
-        ))
-        table(
-            key1 => 143,
-            key2 => << 1, 2, table( 
-                nested => #[myconst],
-            ), 4, >>,
-        )
-        << table(key => 1), 3, 4, >>
+        (def const1 4)
+        (def const2 << 1, 2, #[const1], >> )
+        (def const3 table(
+            key1 => #[const2], 
+            key2 => #[const1],
+        ) )
         """
         result = self.parser.process_config(input_text)
-        expected = [{
-            'key1': 143,
-            'key2': [1, 2, {'nested': {'leeknow':2511}}, 4]
-        }, [{'key': 1}, 3, 4]]
+        expected = {
+            'const1': 4,
+            'const2': [1, 2, 4],
+            'const3': {'key1': [1, 2, 4], 'key2': 4}
+        }
         self.assertEqual(yaml.safe_load(result), expected)
 
 if __name__ == "__main__":
